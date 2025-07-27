7439f68f4df944303cf00399b969db3c5f51d36e
aditiii08 <aditi.20246008@mnnit.ac.in>
2025-07-28 01:28:39 +0530

    Fix footer import path

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import { AboutSection } from "../components/AboutSection";
 import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/footer";
+import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ProjectSection } from "../components/ProjectSection";
